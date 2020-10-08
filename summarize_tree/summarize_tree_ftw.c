b3e0ff5bc01e3b4396000fcb943bf148cb00c64c
Legit0r <michaelfairbanks5469@gmail.com>
2020-10-07 22:25:16 -0500

    summarize_tree_ftw.c

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,32 @@
+#include<ftw.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+static int num_dirs, num_regular;
+static int callback(const char *fpath, const struct stat*sb, int typeflag){
+	  if(typeflag == FTW_D){
+		  	num_dirs++;
+			  }	
+	    if(typeflag == FTW_F){ 
+		      	num_regular++;
+			  }
+	      
+	      return 0; 
+}
+
+#define MAX_FTW_DEPTH 16
+
+int main(int argc, char** argv){
+	  
+	  if(argc != 2){ 
+		      printf("need two arguements");
+		          exit(1);
+			    }
+	    num_dirs = 0; 
+	      num_regular =0; 
+	        ftw(argv[1], callback, MAX_FTW_DEPTH);
+		  printf("There were %d directories.\n" , num_dirs);
+		    printf("There were %d regular files.\n", num_regular);
+
+		      return 0;
+}
